dd7f0f0ce895839ef14d633375177e1f4457158c
pdhoward <patrick.howard@hotmail.com>
2019-05-22 14:39:30 -0400

    updates

--- models/message.js
+++ models/message.js
@@ -3,20 +3,14 @@
 const platform = "+19148195104"
 
 exports.obj = {  
-  From: "",
-  To: platform,
-  Body: "",  
-  FromCity:"Charlotte",
-  FromState: "NC",
-  FromZip: "28222",
-  FromCounty: "USA",  
-  ToCity: "Charlotte",
-  ToState: "NC",
-  ToZip: "28222",
-  ToCountry: "USA",  
+  OwnerId: "",
+  PlatformId: platform,
+  TemplateId: "",
+  Experiences: [],
+  Configs: [],
+  Channels: [],
+  Content: [],   
   ApiVersion: "v1",  
-  Ip: "127.0.0..0",  
-  Channel: "sms",
-  Reply: [],
+  Ip: "127.0.0..0",    
   Token: undefined    
 }
