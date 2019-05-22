ced748de4f7b9c69a1e68a3be9dfe9d1650fcdd0
pdhoward <patrick.howard@hotmail.com>
2019-05-22 13:16:03 -0400

    updates

---
+++ models/message.js
@@ -0,0 +1,22 @@
+
+//default identifier for Machine platform
+const platform = "+19148195104"
+
+exports.obj = {  
+  From: "",
+  To: platform,
+  Body: "",  
+  FromCity:"Charlotte",
+  FromState: "NC",
+  FromZip: "28222",
+  FromCounty: "USA",  
+  ToCity: "Charlotte",
+  ToState: "NC",
+  ToZip: "28222",
+  ToCountry: "USA",  
+  ApiVersion: "v1",  
+  Ip: "127.0.0..0",  
+  Channel: "sms",
+  Reply: [],
+  Token: undefined    
+}
