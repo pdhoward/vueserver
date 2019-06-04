e20f46eeafd10edf63558887eaa10d00a2648fa9
pdhoward <patrick.howard@hotmail.com>
2019-06-04 09:19:58 -0400

    updates

--- reduce.js
+++ reduce.js
@@ -25,3 +25,11 @@ const msgObj = peopleArr
     .reduce(chainedFetchMessages, Promise.resolve({}))
     .then(console.log);
 // ⦘ {glestrade: [ … ], mholmes: [ … ], iadler: [ … ]}
+
+
+// traverse arrays 
+const readings = [0.3, 1.2, 3.4, 0.2, 3.2, 5.5, 0.4];
+const maxReading = readings.reduce((x, y) => Math.max(x, y), Number.MIN_VALUE);
+const minReading = readings.reduce((x, y) => Math.min(x, y), Number.MAX_VALUE);
+console.log({minReading, maxReading});
+// ⦘ {minReading: 0.2, maxReading: 5.5}
