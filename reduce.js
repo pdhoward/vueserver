dd2da23dc38dc9a0443a4c8007db6d15f6a5f6b4
pdhoward <patrick.howard@hotmail.com>
2019-06-04 09:25:23 -0400

    updates

--- reduce.js
+++ reduce.js
@@ -51,7 +51,7 @@ function flatMap(f, arr) {
 const investigators = flatMap(x => x.split(','), fileLines);
 console.log(investigators);
 
-[
+//[
     //   "Inspector Algar",
     //   "Inspector Bardle",
     //   "Mr. Barker",
@@ -69,3 +69,60 @@ console.log(investigators);
     //   "Inspector Stanley Hopkins",
     //   "Inspector Athelney Jones"
     // ]
+
+    // convert and array to object
+
+    const peopleArr  = [
+        {
+            username:    'glestrade',
+            displayname: 'Inspector Lestrade',
+            email:       'glestrade@met.police.uk',
+            authHash:    'bdbf9920f42242defd9a7f76451f4f1d',
+            lastSeen:    '2019-05-13T11:07:22+00:00',
+        },
+        {
+            username:    'mholmes',
+            displayname: 'Mycroft Holmes',
+            email:       'mholmes@gov.uk',
+            authHash:    'b4d04ad5c4c6483cfea030ff4e7c70bc',
+            lastSeen:    '2019-05-10T11:21:36+00:00',
+        },
+        {
+            username:    'iadler',
+            displayname: 'Irene Adler',
+            email:       null,
+            authHash:    '319d55944f13760af0a07bf24bd1de28',
+            lastSeen:    '2019-05-17T11:12:12+00:00',
+        },
+    ];
+
+    function keyByUsernameReducer(acc, person) {
+        return {...acc, [person.username]: person};
+    }
+    const peopleObj = peopleArr.reduce(keyByUsernameReducer, {});
+    console.log(peopleObj);
+    // ⦘ {
+    //     "glestrade": {
+    //         "username":    "glestrade",
+    //         "displayname": "Inspector Lestrade",
+    //         "email":       "glestrade@met.police.uk",
+    //         "authHash":    "bdbf9920f42242defd9a7f76451f4f1d",
+    //          "lastSeen":    "2019-05-13T11:07:22+00:00"
+    //     },
+    //     "mholmes": {
+    //         "username":    "mholmes",
+    //         "displayname": "Mycroft Holmes",
+    //         "email":       "mholmes@gov.uk",
+    //         "authHash":    "b4d04ad5c4c6483cfea030ff4e7c70bc",
+    //          "lastSeen":    "2019-05-10T11:21:36+00:00"
+    //     },
+    //     "iadler":{
+    //         "username":    "iadler",
+    //         "displayname": "Irene Adler",
+    //         "email":       null,
+    //         "authHash":    "319d55944f13760af0a07bf24bd1de28",
+    //          "lastSeen":    "2019-05-17T11:12:12+00:00"
+    //     }
+    // }
+
+
