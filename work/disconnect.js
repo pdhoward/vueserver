c4f99a70ccc6c897bc4d9b9161d995a3f0aec4f6
pdhoward <patrick.howard@hotmail.com>
2019-06-14 13:50:57 -0400

    capture sample components:

---
+++ work/disconnect.js
@@ -0,0 +1,15 @@
+
+
+// handle the array of sockets connected (see machines)
+// for use of Map and Set 
+var allClients = [];
+io.sockets.on('connection', function(socket) {
+   allClients.push(socket);
+
+   socket.on('disconnect', function() {
+      console.log('Got disconnect!');
+
+      var i = allClients.indexOf(socket);
+      allClients.splice(i, 1);
+   });
+});
